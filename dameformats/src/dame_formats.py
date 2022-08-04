ba8ffa4f22b2a3dfd0a3bef2a6ed0d076fba367f
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-08-03 23:48:56 -0300

    src/dame_formats.py: replacing GPL.txt by LICENSE

--- dameformats/src/dame_formats.py
+++ dameformats/src/dame_formats.py
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with Dameformats in the file GPL.txt.  If not, see
+# along with Dameformats in the file LICENSE.  If not, see
 # <https://www.gnu.org/licenses/>.
 
 
